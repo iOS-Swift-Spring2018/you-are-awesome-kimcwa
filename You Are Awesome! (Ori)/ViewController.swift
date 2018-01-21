0a4e78410768e0803992358422fbe58e46b4f8ef
Bryan <kimcwa@bc.edu>
2018-01-21 14:59:27 -0500

    Initial Commit

---
+++ You Are Awesome! (Ori)/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  You Are Awesome! (Ori)
+//
+//  Created by Bryan Kim on 1/21/18.
+//  Copyright © 2018 Bryan Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
