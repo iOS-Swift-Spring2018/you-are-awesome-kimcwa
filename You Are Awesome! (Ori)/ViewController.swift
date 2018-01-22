edb423f1725788f47f902400defe4ae175059128
Bryan <kimcwa@bc.edu>
2018-01-22 17:19:36 -0500

    First file Upload - You Are Awesome

--- You Are Awesome! (Ori)/ViewController.swift
+++ You Are Awesome! (Ori)/ViewController.swift
@@ -10,16 +10,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    var index = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func showMessageButtonPresses(_ sender: UIButton) {
+        
+        let messages = ["You are Fantastic", "You are Great", "You are Amazing", "You are Cool", "I need another Class"]
+    
+        
+        messageLabel.text = messages[index]
+        index += 1
+        if index > messages.count - 1 {
+            index = 0
+        }
+        
+//        let message1 = "You are Awesome"
+//        let message2 = "You are Great"
+//        let message3 = "You are Amazing"
+//        let message4 = "You are Fabulous"
+//        if messageLabel.text == message1 {
+//            messageLabel.text = message2
+//            messageLabel.textColor = UIColor.blue
+//        } else if messageLabel.text == message2 {
+//            messageLabel.text = message4
+//            messageLabel.textColor = UIColor.darkGray
+//        } else if messageLabel.text == message4 {
+//            messageLabel.text = message3
+//            messageLabel.textColor = UIColor.brown
+//        } else {
+//            messageLabel.text = message1
+//            messageLabel.textColor = UIColor.red
+//        }
     }
-
+ 
 
 }
 
